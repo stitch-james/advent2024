52379976a54ebee77beb987488ad2cf31d69a78e
James Allen <james@stitchhealth.co>
2024-12-09 18:50:32 +0000

    Day 9, part 2

--- src/days/day09/index.ts
+++ src/days/day09/index.ts
@@ -4,11 +4,16 @@ function isNull(value: number | null) {
   return !value && value !== 0;
 }
 
+interface Block {
+  start: number
+  length: number
+}
+
 export class Day09 extends Day {
   dayInt = 9;
 
   doPart1(data: string[]) {
-    const blocks = this.parseMap(data);
+    const blocks = this.parseMap1(data);
     let posTo = 0;
     let posFrom = blocks.length - 1;
     while (true) {
@@ -28,24 +33,79 @@ export class Day09 extends Day {
   }
 
   doPart2(data: string[]) {
-    return 0;
+    let { files, free } = this.parseMap2(data);
+    [...files].reverse().forEach((fileBlock) => {
+      for (let freeIndex = 0; freeIndex < free.length; freeIndex++) {
+        const freeBlock = free[freeIndex];
+        if (freeBlock.start > fileBlock.start) {
+          return;
+        }
+        if (freeBlock.length >= fileBlock.length) {
+          fileBlock.start = freeBlock.start;
+          if (freeBlock.length === fileBlock.length) {
+            free = [
+              ...free.slice(0, freeIndex),
+              ...free.slice(freeIndex + 1),
+            ];
+          } else {
+            free = [
+              ...free.slice(0, freeIndex),
+              {
+                start: freeBlock.start + fileBlock.length,
+                length: freeBlock.length - fileBlock.length,
+              },
+              ...free.slice(freeIndex + 1),
+            ];
+          }
+          return;
+        }
+      }
+    });
+    return files.reduce((sum, fileBlock, index) => sum + index * fileBlock.length * (fileBlock.start + (fileBlock.length - 1) / 2), 0);
   }
 
-  parseMap(data: string[]) {
+  parseMap1(data: string[]) {
     const blocks: (number | null)[] = [];
     let id = 0;
-    let free = false;
+    let isFree = false;
     [...data[0]].forEach(char => {
-      const value = free ? null : id;
+      const value = isFree ? null : id;
       const length = parseInt(char, 10);
       if (length > 0) {
         blocks.push(...[...Array(length).keys()].map(() => value));
       };
-      if (!free) {
+      if (!isFree) {
         id = id + 1;
       }
-      free = !free;
+      isFree = !isFree;
     })
     return blocks;
+  }
+
+  parseMap2(data: string[]) {
+    const files: Block[] = [];
+    const free: Block[] = [];
+    let start = 0;
+    let id = 0;
+    let isFree = false;
+    [...data[0]].forEach(char => {
+      const length = parseInt(char, 10);
+      const block = {
+        start,
+        length,
+      };
+      if (isFree) {
+        free.push(block);
+      } else {
+        files.push(block);
+        id = id + 1;
+      }
+      isFree = !isFree;
+      start = start + length;
+    })
+    return {
+      files,
+      free,
+    };
   }
 };
